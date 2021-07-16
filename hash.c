a9859e975b012e9de0319d7aaad7918f58bdfc79
Brendan Jurd <direvus@gmail.com>
2021-07-16 21:14:16 +1000

    Update shimmy2 hash implementation.
    I was not happy about information loss -- once we have processed 32
    characters from the source string, the bits that were shifted off the
    left of the hash were effectively gone.  So two different strings that
    happened to end in the same 32 characters would yield the same hash.
    
    I modified the algorithm to fold the bit that is shifted off into the
    incoming byte.  I also changed my mind about multiplying by the N+1 byte
    and decided to multiply by the N-1 byte instead.

--- hash.c
+++ hash.c
@@ -1,28 +1,47 @@
+#include <limits.h>
 #include <string.h>
 
 /*
- * Compute an unsigned long integer hash from a NUL-terminated byte array.
+ * Compute an unsigned long integer hash from a byte array.
  *
- * If the argument is a NULL pointer, or an empty string, the hash is zero.  If
- * the argument contains one byte, then the hash is just the integer value
- * of that byte.  Otherwise, for each byte we bitshift the hash one bit left,
- * then add the value of that byte minus one, multiplied by the value of the
- * following byte.
+ * If 'bytes' is a NULL pointer, or 'len' is less than 1, the hash is zero.
+ *
+ * If 'len' is 1, then the hash is just the integer value of the first byte.
+ * Otherwise, we read 'len' bytes from 'bytes', and for each byte we save the
+ * leftmost bit of the hash, bitshift the hash one bit left, then add the value
+ * of the new byte, less the value of the bit that was shifted off, multiplied
+ * by the value of the preceding byte (if not the first byte).
  */
-unsigned long hash_shimmy2(const char *bytes) {
-    if (!bytes) {
-        return 0;
-    }
+unsigned long hash_shimmy2_len(const char *bytes, const size_t len) {
     unsigned long result = 0;
-    const size_t len = strlen(bytes);
+    unsigned char shift = 0;
+    unsigned int ch = 0;
+    const size_t bits = (sizeof result) * CHAR_BIT - 1;
     if (len == 0) {
         return 0;
     } else if (len == 1) {
         return bytes[0];
     }
-    for (unsigned int i = 0; i < len - 1; i++) {
+    for (size_t i = 0; i < len; i++) {
+        shift = result >> bits;
         result <<= 1;
-        result += bytes[i+1] * (bytes[i] - 1);
+        ch = bytes[i] - shift;
+        if (i > 0)
+            ch *= bytes[i - 1];
+        result += ch;
     }
     return result;
+}
+
+/*
+ * Compute an unsigned long integer hash from a NUL-terminated byte array.
+ *
+ * As for hash_shimmy2_len(), except we consume bytes from 'bytes' until we
+ * encounter a zero byte.
+ */
+unsigned long hash_shimmy2(const char *bytes) {
+    if (!bytes) {
+        return 0;
+    }
+    return hash_shimmy2_len(bytes, strlen(bytes));
 }
